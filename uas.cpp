0a35fe9df69b72d2336630cde69104871743c56f
xLeeMoon <razibaditya85@gmail.com>
2024-07-18 13:45:30 +0800

    fix : cin.ignore();

--- uas.cpp
+++ uas.cpp
@@ -13,4 +13,12 @@ int main(){
     vector<Item>item;
     char choice;
     double total = 0.0;
+
+    cout << "Penghitung Hasil Toko\n";
+
+    do{
+        Item item;
+        cout << "\nMasukkan nama barang:";
+        cin.ignore();
+    }
 }
